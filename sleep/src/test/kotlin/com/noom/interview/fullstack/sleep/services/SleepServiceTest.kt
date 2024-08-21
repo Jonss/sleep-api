b73df0edfb7aed7133a2965ac496be9df2e95c56
joao santana <joaosantana.ti@gmail.com>
2024-08-21 19:17:47 -0300

    test: last night sleep

--- sleep/src/test/kotlin/com/noom/interview/fullstack/sleep/services/SleepServiceTest.kt
+++ sleep/src/test/kotlin/com/noom/interview/fullstack/sleep/services/SleepServiceTest.kt
@@ -1,5 +1,50 @@
 package com.noom.interview.fullstack.sleep.services
 
-import org.junit.jupiter.api.Assertions.*
+import com.nhaarman.mockitokotlin2.any
+import com.noom.interview.fullstack.sleep.repositories.SleepLogRepository
+import com.noom.interview.fullstack.sleep.services.stubs.singleSleepLogsStub
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.mockito.junit.jupiter.MockitoExtension
 
-class SleepServiceTest
+@ExtendWith(MockitoExtension::class)
+class SleepServiceTest {
+    @Mock
+    lateinit var mockRepository: SleepLogRepository
+
+    @InjectMocks
+    private lateinit var sleepService: SleepService
+
+    @Test
+    fun shouldGetLastNightSleepLog() {
+        // given
+        `when`(mockRepository.fetchByUserIdFromInterval(any(), any(), any())).thenReturn(singleSleepLogsStub)
+
+        // when
+        val sleepLog = sleepService.getLastNightSleepLog(1)
+
+        // then
+        assertNotNull(sleepLog)
+        verify(mockRepository, times(1)).fetchByUserIdFromInterval(any(), any(), any())
+    }
+
+    @Test
+    fun shouldNotGetLastNightSleepLogWhenListIsEmpty() {
+        // given
+        `when`(mockRepository.fetchByUserIdFromInterval(any(), any(), any())).thenReturn(emptyList())
+
+        // when
+        val sleepLog = sleepService.getLastNightSleepLog(1)
+
+        // then
+        assertNull(sleepLog)
+        verify(mockRepository, times(1)).fetchByUserIdFromInterval(any(), any(), any())
+    }
+}
--- sleep/src/test/kotlin/com/noom/interview/fullstack/sleep/services/stubs/SleepStubs.kt
+++ sleep/src/test/kotlin/com/noom/interview/fullstack/sleep/services/stubs/SleepStubs.kt
@@ -1,2 +1,20 @@
 package com.noom.interview.fullstack.sleep.services.stubs
 
+import com.noom.interview.fullstack.sleep.models.dtos.SleepLogDTO
+import com.noom.interview.fullstack.sleep.models.entities.User
+import com.noom.interview.fullstack.sleep.models.enums.SleepQuality
+import com.noom.interview.fullstack.sleep.utils.yesterday
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+val singleSleepLogsStub =
+    listOf(
+        SleepLogDTO(
+            id = 1,
+            user = User(1, "id"),
+            startDate = yesterday.plus(16, ChronoUnit.HOURS),
+            endDate = yesterday.plus(27, ChronoUnit.HOURS),
+            quality = SleepQuality.OK,
+            createdAt = Instant.now(),
+        ),
+    )
